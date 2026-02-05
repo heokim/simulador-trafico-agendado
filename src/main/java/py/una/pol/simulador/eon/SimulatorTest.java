0240dc2f74ad9d2da77b39e0bb4e770f40c0d173
andresheokim <57774541+heokim@users.noreply.github.com>
2026-02-05 12:10:22 -0300

    correcion de validacion de XT

--- src/main/java/py/una/pol/simulador/eon/SimulatorTest.java
+++ src/main/java/py/una/pol/simulador/eon/SimulatorTest.java
@@ -64,11 +64,11 @@ public class SimulatorTest {
     public static void main(String[] args) throws SQLException, IOException {
         TOPOLOGY = TopologiesEnum.USNET;
 
-        ERLANG = 1800;
+        ERLANG = 2100;
         DESCRIPCION = "Dinamico, KSP por uso de FS, core por uso fs";
         T_RANGE_MIN = 0;
         T_RANGE_MAX = 0;
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 10; i++) {
             simular();
         }
 
