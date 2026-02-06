b4eb6791b178ab132be6534dcf82cf91aea77ad3
andresheokim <57774541+heokim@users.noreply.github.com>
2026-02-05 21:46:52 -0300

    correcion de validacion de XT

--- src/main/java/py/una/pol/simulador/eon/rsa/Algorithms.java
+++ src/main/java/py/una/pol/simulador/eon/rsa/Algorithms.java
@@ -691,7 +691,7 @@ public class Algorithms {
                 }
 
                 // --- Validaciones Globales (Whole Path Consistency) ---
-
+                /*
                 List<List<FrequencySlot>> testBlocks = new ArrayList<>(currentBlocks);
                 testBlocks.add(block);
                 List<Link> testLinks = new ArrayList<>(currentLinks);
@@ -715,6 +715,7 @@ public class Algorithms {
                     result.setCrosstalkError(true);
                     continue;
                 }
+                */
 
                 // --- Asignación Exitosa para este Enlace ---
                 currentCores.add(core);
